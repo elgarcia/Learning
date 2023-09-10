60b228e357605542f2c123dedf13f55a0bdeebb2
eliagarc <elias16199@gmail.com>
2023-09-10 12:41:26 +0200

    C# tests and lessons

--- C#/CsharpProjects/TestProject/Program.cs
+++ C#/CsharpProjects/TestProject/Program.cs
@@ -1,8 +1,24 @@
-﻿using System;
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
+﻿Random random = new Random();
 
-largerValue = Math.Max(firstValue, secondValue);
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-Console.WriteLine(largerValue);
+//Your code goes here
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    Console.WriteLine($"Renew now and save 2{discountPercentage}%");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine($"Renew now and save 1{discountPercentage}%");
+}
+else if(daysUntilExpiration <= 10)
+{
+   Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
---
+++ Javascript/QuizGame/app.js
@@ -0,0 +1,14 @@
+let count;
+
+const quests = document.querySelector("#question");
+const answers = document.querySelectorAll(".btn");
+
+console.log(answers);
+
+answers.forEach(function(answers)
+{
+    answers.addEventListener("click", function(e)
+    {
+        const styles = document.get
+    });
+});
